741c350094bccf716a9f8d5a0e57995156f208ee
Vasya Yasyuk <smoyukmekj@mail.ru>
2024-05-11 18:07:54 +0300

    Добавьте файлы проекта.

---
+++ Project5/MyForm1.h
@@ -0,0 +1,124 @@
+#pragma once
+
+namespace Project5 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// —водка дл€ MyForm1
+	/// </summary>
+	public ref class MyForm1 : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm1(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// ќсвободить все используемые ресурсы.
+		/// </summary>
+		~MyForm1()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^ button1;
+	protected:
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::TextBox^ textBox1;
+
+	private:
+		/// <summary>
+		/// ќб€зательна€ переменна€ конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// “ребуемый метод дл€ поддержки конструктора Ч не измен€йте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->DialogResult = System::Windows::Forms::DialogResult::OK;
+			this->button1->Location = System::Drawing::Point(32, 137);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(141, 41);
+			this->button1->TabIndex = 5;
+			this->button1->Text = L"OK";
+			this->button1->UseVisualStyleBackColor = false;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm1::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(247, 137);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(129, 41);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"ќтмен";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm1::button2_Click);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(59, 53);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(285, 13);
+			this->label1->TabIndex = 5;
+			this->label1->Text = L"¬ведите целое число (высота строки/ширина столбца)";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(32, 86);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(344, 20);
+			this->textBox1->TabIndex = 6;
+			// 
+			// MyForm1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(394, 202);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
+			this->Name = L"MyForm1";
+			this->Text = L"MyForm1";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		int h, m;
+		h = Convert::ToInt32(textBox1->Text); m = Convert::ToInt32(textBox1->Text);
+	}
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+		this->Hide();
+	}
+};
+}
